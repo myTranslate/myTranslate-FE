b9e2515ba81fa067cbc033144affbbdf361f56e0
rubythalib33 <rubyabdullah14@gmail.com>
2022-03-18 11:12:26 +0700

    update card

--- pages/index.vue
+++ pages/index.vue
@@ -6,19 +6,43 @@
         <v-card>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Card Title</h3>
-              <div>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-              </div>
+              <v-row>
+                <v-col cols="15" md="10">
+                <template>
+                  <v-tabs>
+                    <v-tab
+                      v-for="item in languages"
+                      :key="item"
+                    >
+                      {{ item }}
+                    </v-tab>
+                  </v-tabs>
+                </template>
+                </v-col>
+                <v-col cols="15" md="1"><v-spacer></v-spacer></v-col>
+                <v-col cols="15" md="1">
+                <!-- button mdi switch -->
+                <v-btn
+                  v-if="languages.length > 1"
+                  @click="switchLanguage"
+                  icon
+                >
+                  <v-icon>mdi-swap-horizontal</v-icon>
+                </v-btn>
+                </v-col>
+              </v-row>
             </div>
           </v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Mauris sagittis pellentesque lacus eleifend lacinia...
+            <!-- create text area -->
+            <v-textarea
+               v-model="text"
+                label="Message"
+                counter
+                maxlength="200"
+                full-width
+                single-line
+            ></v-textarea>
           </v-card-text>
         </v-card>
       </v-col>
@@ -26,31 +50,70 @@
         <v-card>
           <v-card-title primary-title>
             <div>
-              <h3 class="headline mb-0">Card Title</h3>
-              <div>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-                <v-icon>star</v-icon>
-              </div>
+              <v-tabs>
+                  <v-tab
+                    v-for="item in languages"
+                    :key="item"
+                  >
+                    {{ item }}
+                  </v-tab>
+                </v-tabs>
             </div>
           </v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Mauris sagittis pellentesque lacus eleifend lacinia...
+            <v-container>
+              {{ text }}
+            </v-container>
           </v-card-text>
+          <!-- add copy button on right corner -->
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              @click="copyText"
+              icon
+            >
+              <v-icon>mdi-content-copy</v-icon>
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-col>
+    </v-row>
+    <!-- Create primary button on right corner with name Translate -->
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-btn
+        @click="translate"
+        color="#27D3EB"
+        dark
+        large
+        class="ma-2"
+        right
+      >
+        Translate
+      </v-btn>
     </v-row>
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({
-      reveal: false,
-    }),
+    data () { 
+      return{
+      languages : ['Indonesia', 'English', 'Arabic'],
+      selectedLanguage : 'Indonesia',
+      tobeTranslateLanguage : 'English',
+      text : '',
+    }
+    },
+    methods: {
+      switchLanguage: function() {
+        this.tobeTranslateLanguage = this.selectedLanguage;
+        this.selectedLanguage = this.languages[0];
+      },
+      copyText: function() {
+        navigator.clipboard.writeText(this.text);
+      }
+    }
   }
 </script>
 
@@ -60,5 +123,10 @@
   align-items: center;
   justify-content: center;
   transform: translate(0%, -10%);
+}
+.v-container {
+  height: 100%;
+  display: flex;
+  max-width: 500px;
 }
 </style>
