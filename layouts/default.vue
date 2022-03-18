683546bccd097f561f7cf6bdd210a25ccc02cd9e
rubythalib33 <rubyabdullah14@gmail.com>
2022-03-18 09:40:30 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      :clipped-left="clipped"
+      color="#27D3EB"
+      fixed
+      app
+    >
+      <v-toolbar-title  ><h1>my<strong>Translate</strong></h1></v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* change v-app-bar to #27D3EB */
+.v-app-bar {
+  color: #fff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <!-- create 2 card in one row -->
+    <v-row class="center">
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Card Title</h3>
+              <div>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+              </div>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris sagittis pellentesque lacus eleifend lacinia...
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Card Title</h3>
+              <div>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+                <v-icon>star</v-icon>
+              </div>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris sagittis pellentesque lacus eleifend lacinia...
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      reveal: false,
+    }),
+  }
+</script>
+
+<style>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform: translate(0%, -10%);
+}
+</style>
